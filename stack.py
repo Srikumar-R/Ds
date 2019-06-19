0dfb688defd7381af3ac40e3793a042137aa2ab4
Srikumar-R <42167151+Srikumar-R@users.noreply.github.com>
2019-06-19 12:21:32 +0530

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,7 +1,9 @@
-def insert(q,w):
+def push(q,w):
     q.append(w)
-def delete(q):
+def pop(q):
     q.pop()
+def peek(q):
+    return(q[-1])
 def search(q,w):
     if w in q:
         return("YES")
@@ -17,13 +19,14 @@ z=True
 while(z==True):
     print("Press \"1\" for \"Insert in a list\" \nPress \"2\" for \"Delete in list\"")
     print("Press \"3\" for \"Search in a list\"\nPress \"4\" for \"Display values in a list\"\nPress \"5\" to \"Exit the list process\"")
+    print("Press \"6\" for \"Peeking value in a list\"")
     b=input()
     if b=='1':
         x=input("Enter the data to be inserted: ")
-        insert(a,x)
+        push(a,x)
         print_values(a)
     elif b=='2' and a!=[]:
-        delete(a)
+        pop(a)
         print_values(a)
     elif b=='3' and a!=[]:
         x=input("Enter the data to be searched: ")
@@ -36,10 +39,12 @@ while(z==True):
         print_values(a)
     elif b=='5':
         z=False
+    elif b=='6':
+        print(peek(a))
     else:
         if a==[]:
             print("List is empty...")
-        elif b not in '12345':
+        elif b not in '123456':
             print("Invalid input")
 print("Process Finished... Thank you Visit Again..!!!")
 
